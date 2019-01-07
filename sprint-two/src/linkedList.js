354bc1a400c164587ec43ae0f7478941be712e99
arnold1105 <arnold.b.cheung@gmail.com>
2019-01-07 11:03:51 -0800

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head) {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return undefined;
+    }
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+  
   };
 
   list.contains = function(target) {
+    if (list.head) {
+      if (list.head.value === target) {
+        return true;
+      }
+      list.head = list.head.next;
+      return list.contains(target);
+    } else {
+      return false;
+    }
   };
 
   return list;
