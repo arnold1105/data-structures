80966fb629f6f1969623134b02aabb90df9094f1
arnold1105 <arnold.b.cheung@gmail.com>
2019-01-07 22:21:38 -0800

    unfinished binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,34 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  newTree.left = [];
+  newTree.right = [];
+  _.extend(newTree, treeMethods);
+  
+  return newTree;
 };
+
+var treeMethods = {};
+
+treeMethods.insert = function(value) {
+  //if the value is smaller than this.value, push to left array
+  if (value < this.value) {
+    this.left.push(value);
+  } else {
+    this.right.push(value);
+  }
+  //if larger     ,, push to right array;
+};
+
+
+treeMethods.contains = function(item) {
+  //recursion
+};
+
+treeMethods.depthFirstLog = function(cb) {
+  //
+};
+
 
 
 /*
